259504f76eb4c1b0c80ca06d97c453838bb6cf19
MoonLGH <MoonLMC1212@gmail.com>
2022-12-22 10:01:49 +0000

    found random key lol

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '25bc90a1196e6f153eece0bc0b0fc9eb', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
